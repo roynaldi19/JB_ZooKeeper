5b38e25d7427c5ae020377cddacc018a313cc562
roynaldi19 <roynaldi19@gmail.com>
2023-06-07 13:28:23 +0700

    Initial commit

---
+++ Zookeeper (Kotlin)/task/src/Zookeeper.kt
@@ -0,0 +1,25 @@
+var camel = """Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+
+fun main() {
+    println(camel)
+
+}
---
+++ Zookeeper (Kotlin)/task/test/ZookeeperTest.kt
@@ -0,0 +1,39 @@
+import org.hyperskill.hstest.dynamic.DynamicTest
+import org.hyperskill.hstest.stage.StageTest
+import org.hyperskill.hstest.testcase.CheckResult
+import org.hyperskill.hstest.testing.TestedProgram
+
+class ZookeeperTest : StageTest<String>() {
+
+    val camel = """Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+    @DynamicTest
+    fun test(): CheckResult {
+        val testedProgram = TestedProgram();
+        val output = testedProgram.start().trim();
+
+        if (output != camel) {
+            return CheckResult.wrong("You should output a camel!");
+        }
+
+        return CheckResult.correct()
+    }
+}
